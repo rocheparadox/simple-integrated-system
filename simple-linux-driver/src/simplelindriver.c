248a59e7fa8b0cad3a1ff6f0e9b45b42466d547b
rocheparadox <rocheinside@gmail.com>
2024-09-15 15:35:08 -0400

    Added I2C device initialization and destruction code
    - An I2C object to communicate with the ARM controller is created during
      init function
    - The I2C driver is then destroyed when the module is removed from the
      kernel

--- simple-linux-driver/src/simplelindriver.c
+++ simple-linux-driver/src/simplelindriver.c
@@ -2,9 +2,36 @@
 #include<linux/init.h>
 #include<linux/fs.h>
 #include<linux/cdev.h>
+#include<linux/i2c.h>
 
-MODULE_LICENSE("GPL2");
+MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Roche Christopher");
+
+
+/* Function Declarations */
+//int init_arm_i2c_device(void);
+
+
+/* I2C details */
+#define ARM_I2C_ADDRESS 0x30 // TODO: Should be changed later
+#define ARM_I2C_SLAVE_NAME "ARM I2C DEVICE"
+
+/* Define the I2C device details */
+
+struct i2c_adapter *arm_i2c_adapter;
+struct i2c_client *arm_i2c_client;
+    /* Create an I2C device client */
+    struct i2c_board_info arm_i2c_board_info = {
+	    I2C_BOARD_INFO(ARM_I2C_SLAVE_NAME, ARM_I2C_ADDRESS)
+    };
+
+    struct i2c_driver arm_i2c_driver = {
+   	.driver = {
+		.owner = THIS_MODULE,
+       		.name = ARM_I2C_SLAVE_NAME 
+	}	
+    };
+int arm_i2c_device_registered = 0;
 
 dev_t dev;
 int device_count = 1;
@@ -12,31 +39,73 @@ char* device_name = "simple_linux_driver";
 
 struct cdev simple_char_device;
 
-// /* File operations */
-// static int driver_read(){
-//     return 0;
-// }
+/* I2C device initialization function */
 
-// /* This method gets called when something is written to this device*/
-// static int driver_write(){
-//     return 0;
-// }
+
+int init_arm_i2c_device(void){
+
+    arm_i2c_adapter = i2c_get_adapter(1);
+    /* Create the linux device */    
+    if(arm_i2c_adapter == NULL){
+    	printk(KERN_ERR "\nI2C adapter cannot be created. Most likely, no i2c bus is available in the device\n");
+	return -1;
+    }
+
+    arm_i2c_client = i2c_new_client_device(arm_i2c_adapter, &arm_i2c_board_info); 
+    if(arm_i2c_client == NULL){
+	    printk(KERN_ERR "A client device for arm i2c communication cannot be created\n");
+	    i2c_put_adapter(arm_i2c_adapter); 
+	    return -1;
+    }
+    
+    /* Add the i2c driver to the system */
+    if(i2c_add_driver(&arm_i2c_driver) != -1){
+    	printk(KERN_INFO "I2C driver for ARM has been added to the system\n");
+   	arm_i2c_device_registered = 1;  
+    }
+    else{
+    	printk(KERN_ERR "I2C driver for ARM cannot be added to the system");
+    }
+
+    return 0;
+}
+
+void arm_i2c_device_cleanup(void){
+	if(arm_i2c_device_registered){
+		printk(KERN_INFO "Going to delete the driver %s\n", ARM_I2C_SLAVE_NAME);
+		i2c_del_driver(&arm_i2c_driver);	
+	}
+}
+
+
+/* File operations */
+
+/* This method gets called when something is written to this device*/
+ssize_t driver_write(struct file *filep, const char __user *buff, size_t count, loff_t *offp){
+    printk("The following was written to the device : ");
+    for(int i=0; i<count; i++){
+    	printk(KERN_CONT "%c", buff[i]); 
+    }   
+   printk("\n");
+  return count; 
+}
 
 /* This method gets called when the device is opened */
 static int driver_open(struct inode *inode, struct file *file_pointer){
-    printk("A device using the simple linux driver was opened");
+    printk("A device using the simple linux driver was opened\n");
     return 0;
 }
 
 /* This method gets called when the device is closed */
 static int driver_close(struct inode *inode, struct file *file_pointer){
-    printk("A device using the simple linux driver was closed");
+    printk("A device using the simple linux driver was closed\n");
     return 0;
 }
 
 static struct file_operations fops = {
     .owner = THIS_MODULE,
     .open = driver_open,
+    .write = driver_write,
     .release = driver_close
 };
 
@@ -50,12 +119,16 @@ static int __init simplelindrvr_init(void){
         printk("An error occurred while trying to initialize the simple linux driver");
     }
 
-    // /* Create a device file */
-    // cdev_init(&simple_char_device, &fops);
-    // simple_char_device.owner = THIS_MODULE;
+     /* Create a device file */
+    cdev_init(&simple_char_device, &fops);
+    simple_char_device.owner = THIS_MODULE;
 
-    // /* Add the device file to the system*/
-    // cdev_add(&simple_char_device, 0, 1);
+    /* Add the device file to the system*/
+    cdev_add(&simple_char_device, dev, 1);
+
+    /* Initialize the i2c device */
+    init_arm_i2c_device();
+
 
     return 0;
 }
@@ -66,6 +139,9 @@ static void __exit simplelindrvr_cleanup(void){
     cdev_del(&simple_char_device);
     // unregister the device numbers
     unregister_chrdev_region(dev, device_count);
+    
+    // Delete i2c device
+    arm_i2c_device_cleanup();
     printk("Simple Linux Driver Cleanup done\n");
 }
 
